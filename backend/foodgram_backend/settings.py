6b3fa172712984ae31246989fb9e33e4e8c74b3e
Даниил Пастунов <dflient@icloud.com>
2024-01-25 00:13:37 +0300

    1st

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'recipes',
     'tags',
     'ingridients',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
